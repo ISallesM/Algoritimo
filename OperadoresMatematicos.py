516e99d19d7e15b4625a85bf284cf4951afb3f98
Igor Salles <isallesmari@yahoo.com.br>
2025-07-31 12:46:47 -0300

    Operadores

---
+++ OperadoresMatematicos.py
@@ -0,0 +1,24 @@
+# soma = +
+# subtração = -
+# multiplicação = *
+# divisão = /
+# módulo = % /* retorna o resto da divisão */
+# divisao inteiro = //
+# exponencial = 3**2 = 9
+
+# #Exemplo1 Concatenação Textos
+texto1 = input("Digite texto1:")
+texto2 = input("Digite texto2:")
+print("Resultado de texto1 + texto2 é:",texto1 +"",texto2)
+
+#Exemplo2 soma de inteiros
+valor1 = int(input("Digite valor1: "))
+valor2 = int(input("Digite valor2: "))
+
+
+print(f"A soma de {valor1} + {valor2} é: {valor1 + valor2}")
+print(f"A subtração de {valor1} - {valor2} é: {valor1 - valor2}")
+print(f"A multiplicação de {valor1} * {valor2} é: {valor1 * valor2}")
+print(f"A divisão de {valor1} / {valor2} é: {valor1 / valor2}")
+A =7
+print(f"Tipo de A {type(A)}")
